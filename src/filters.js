fbdf1a76ac990520947d9c3007c7fb87af7f819e
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-06-14 21:49:17 +0800

    Styling

--- src/filters.js
+++ src/filters.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import MarkdownIt from 'markdown-it';
 
+import hljs from 'highlight.js';
+import 'highlight.js/styles/solarized-dark.css';
+
 const unsafeMd = new MarkdownIt({
   html: false,
   linkify: true,
@@ -11,6 +14,15 @@ const safeMd = new MarkdownIt({
   html: true,
   linkify: true,
   typographer: false,
+  highlight: (str, lang) => {
+    if(lang && hljs.getLanguage(lang)) {
+      try {
+        return `<pre class="hljs"><code>${hljs.highlight(lang, str).value}</code></pre>`;
+      } catch(__) { /* ignore */ }
+    }
+
+    return `<pre class="hljs"><code>${safeMd.utils.escapeHtml(str)}</code></pre>`;
+  },
 });
 
 Vue.filter('timeConverter', (time) => {
