86daef182c86150dafb0106ffc35a419605c75bd
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-08-08 14:23:59 +0800

    Added a feed button

--- src/main.js
+++ src/main.js
@@ -594,6 +594,10 @@ const instance = new Vue({
     eventBlocker(e) {
       e.stopPropagation();
     },
+
+    gotoFeed() {
+      window.location.href = `${config.backend}/feed`;
+    },
   },
 });
 
